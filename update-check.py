5b74b55b1a8f149970456ca08679c1f77124e2c1
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-07-22 16:49:01 +0200

    automate version checks

--- update-check.py
+++ update-check.py
@@ -17,7 +17,7 @@ def check_version(directory, new_version):
 
     with open(f"{directory}/VERSION", "r") as f:
         old_version = f.read().rstrip()
-    
+
     print(f"Up-to-date version {new_version}")
     print(f"Current version: {old_version}")
 
@@ -28,10 +28,12 @@ def check_version(directory, new_version):
         with open(f"{directory}/VERSION", "w") as f:
             f.write(new_version)
         # commit
-        result = os.system(f"git config --local user.email 'actions@github.com' \
+        result = os.system(
+            f"git config --local user.email 'actions@github.com' \
             && git config --local user.name 'GitHub Actions' \
             && git add {directory}/VERSION \
-            && git commit -m 'Bump {directory} version to {new_version}'")
+            && git commit -m 'Bump {directory} version to {new_version}'"
+        )
         if result != 0:
             print("Failed to commit the bump. Exiting")
             exit(1)
@@ -46,18 +48,11 @@ def check_version(directory, new_version):
         print(f"Already newest version {old_version}")
 
 
-# check deb version
-response = requests.get("https://protonmail.com/download/current_version_linux.json")
-content = json.loads(response.content)
-version = re.match(".*_([0-9.-]+)_amd64\.deb", content["DebFile"]).group(1)
-check_version("deb", version)
-
-
 # check build version
 response = requests.get(
     "https://api.github.com/repos/ProtonMail/proton-bridge/tags",
     headers={"Accept": "application/vnd.github.v3+json"},
-    )
+)
 tags = json.loads(response.content)
 version_re = re.compile("v\d+\.\d+\.\d+")
 releases = [tag["name"][1:] for tag in tags if version_re.match(tag["name"])]
